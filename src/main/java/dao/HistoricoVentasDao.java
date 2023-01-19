8ac98d927efd31db3ad10e4b28e6c128b33dd0b9
SakyJoestar <itzsaky@outlook.com>
2023-01-19 15:39:11 -0500

    DaoHistoricoVentas cree la clase HistoricoVentasDao (dao) con sus atributos y funcionalidades

---
+++ src/main/java/dao/HistoricoVentasDao.java
@@ -0,0 +1,56 @@
+package dao;
+import modelo.VentaPeriodo;
+
+import java.util.ArrayList;
+
+public class HistoricoVentasDao
+{
+    private ArrayList<VentaPeriodo> historicoVentas;
+
+    public HistoricoVentasDao()
+    {
+        this.historicoVentas = new ArrayList<VentaPeriodo>();
+    }
+
+    public ArrayList<VentaPeriodo> getHistorico()
+    {
+        return historicoVentas;
+    }
+
+    public boolean anhadirVenta(VentaPeriodo auxVenta)
+    {
+        historicoVentas.add(auxVenta);
+        return true;
+    }
+
+    public boolean editarVenta(VentaPeriodo auxVenta)
+    {
+        if(historicoVentas.contains(auxVenta))
+        {
+            int pos = historicoVentas.indexOf(auxVenta);
+            historicoVentas.set(pos,auxVenta);
+            return true;
+        }
+        return false;
+    }
+
+    public boolean eliminarVenta(VentaPeriodo auxVenta)
+    {
+        historicoVentas.remove(auxVenta);
+        return true;
+    }
+
+    public VentaPeriodo getVentaHistorica(int auxId)
+    {
+        VentaPeriodo auxVenta = null;
+        for(VentaPeriodo ventaPeriodo: historicoVentas)
+        {
+            if(ventaPeriodo.getId()== auxId)
+            {
+                auxVenta = ventaPeriodo;
+                break;
+            }
+        }
+        return auxVenta;
+    }
+}
